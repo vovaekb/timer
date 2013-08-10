bcf014638643ce82ee487f26a48dbb992822e865
Klaster_1 <Klaster1@gmail.com>
2013-08-10 19:56:36 +0800

    Remove old requirejs config.

--- app/js/app/main.js
+++ app/js/app/main.js
@@ -1,25 +1,3 @@
-/*require.config({
-	baseUrl: "js",
-	paths: {
-		jQuery: "lib/jquery/jquery",
-		angular: "lib/angular/angular",
-		angularui: "lib/angular-ui/angular-ui",
-		angularuirouter: "lib/angular-ui-router/angular-ui-router",
-		angularbs: "lib/angular-bootstrap/ui-bootstrap-0.6.0-SNAPSHOT",
-		bootstrap: "lib/bootstrap/bootstrap",
-		paginator: "app/paginator",
-		timer: "app/app"
-	},
-	shim: {
-		bootstrap: ["jQuery"],
-		angularui: ["angular", "bootstrap", "angularbs"],
-		angularuirouter: ["angularui"],
-		angularbs: ["angular"],
-		paginator: ["angular"],
-		timer: ["angularuirouter"]
-	}
-});
-*/
 require.config({
 	baseUrl: "js",
 	paths: {
