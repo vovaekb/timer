bcc60d1e2366f0b874044f193ec936e405cee9d8
Klaster_1 <Klaster1@gmail.com>
2013-08-16 15:57:45 +0800

    Work on E2E tests. They aren't working yet, though.

--- test/test-main.js
+++ test/test-main.js
@@ -8,18 +8,19 @@ requirejs.config({
 	baseUrl: "base/app/js",
 	paths: {
 		angular: "lib/angular/angular",
-		// angularui: "lib/angular-ui/angular-ui",
-		// angularuirouter: "lib/angular-ui-router/angular-ui-router",
-		// angularbs: "lib/angular-bootstrap/ui-bootstrap-0.6.0-SNAPSHOT",
+		angularui: "lib/angular-ui/angular-ui",
+		angularuirouter: "lib/angular-ui-router/angular-ui-router",
+		angularbs: "lib/angular-bootstrap/ui-bootstrap-0.6.0-SNAPSHOT",
 		angularmocks: "../../test/angular-mocks",
-		// timer: "app/app"
+		angularscenario: "../../test/angular-scenario",
+		timer: "app/app"
 	},
 	shim: {
-		// angularui: ["angular"],
-		// angularuirouter: ["angular"],
-		// angularbs: ["angular"],
+		angularui: ["angular"],
+		angularuirouter: ["angular"],
+		angularbs: ["angular"],
 		angularmocks: ["angular"],
-		// angularscenario: ["angular"]
+		angularscenario: ["angular"]
 	},
 	deps: tests,
 	callback: window.__karma__.start
