bd1bcc8cbfb9d49cd4ac1176c9e01811d8246175
Klaster_1 <Klaster1@gmail.com>
2013-09-03 17:05:06 +0800

    Update unit tests

--- test/unit/util.spec.js
+++ test/unit/util.spec.js
@@ -23,11 +23,17 @@ describe("timer.util", function () {
 			"range"
 		].sort();
 
-		it("should have Util service", function () {
+		it("has Util service", function () {
 			expect(Util).toBeDefined();
 		});
-		it("should expose specified functions", function () {
+		it("exposes specified functions", function () {
 			expect(Object.keys(Util).sort()).toEqual(keys);
+		});
+		it("sums", function () {
+			var sum = Util.sum;
+			expect(sum(2,2)).toEqual(4);
+			expect(sum("2","2")).toEqual("22");
+			expect(isNaN(sum(2, undefined))).toBe(true);
 		});
 	});
 	describe("controllers", function () {
